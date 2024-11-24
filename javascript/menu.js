9e8f822c696013ec2bdb09dfadbab2f97825cfd6
Thomas Fromont <thomas.fromont@radiofrance.com>
2024-11-24 21:28:54 +0100

    Update menu burger

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,5 +1,6 @@
-// menu.js
 function toggleMenu() {
     const menu = document.getElementById('menu');
+    const burger = document.querySelector('.menu-burger');
     menu.classList.toggle('open');
+    burger.classList.toggle('open');
 }
